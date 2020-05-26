e41ec0794452a58f28b2dfe5d690cf8f7f78754f
ImSajalKaushik <SajalKaushik.sk@gmail.com>
2020-05-26 15:10:51 +0530

    Firebase sign up
    install pod on your own

---
+++ FireBaseDemo/SignupViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SignupViewController.swift
+//  FireBaseDemo
+//
+//  Created by Sajal Kaushik on 26/05/20.
+//  Copyright © 2020 Sajal Kaushik. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    var emailAddress: String? = nil
+    var pass: String? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        email.delegate = self
+        password.delegate = self
+    }
+
+    @IBAction func signUp(_ sender: UIButton) {
+        if emailAddress != nil, pass != nil {
+            Auth.auth().createUser(withEmail: emailAddress!, password: pass!) { (result, err) in
+                if err != nil {
+                    print("error in sign up:", err!.localizedDescription)
+                } else {
+                    let uid = result?.user.uid
+                }
+            }
+        }
+         self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension SignupViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == email {
+            password.becomeFirstResponder()
+            emailAddress = email.text
+            
+        } else {
+            pass = password.text
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
---
+++ FireBaseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseDemo
+//
+//  Created by Sajal Kaushik on 26/05/20.
+//  Copyright © 2020 Sajal Kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
